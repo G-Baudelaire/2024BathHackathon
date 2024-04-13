44d1a38899911a1c1eb17e2b226a3bfa24ea1727
Felix <76658412+felix-jwr@users.noreply.github.com>
2024-04-13 21:04:20 +0100

    Code for fruit controller

---
+++ fruitController.ino
@@ -0,0 +1,54 @@
+float inputA0, inputA1, inputA3, inputA4;   // Input magnitudes on the pins
+const byte PIN_IDENTIFIERS[] = {0x00, 0x01, 0x02, 0x03}; // Identifiers for pins 
+const byte PIN_NUMBERS[] = {A0, A1, A3, A4}; // Pin numbers corresponding to the identifiers (left, right, up, down)
+
+void setup() {
+  // Initialize serial communication at 9600 baud rate
+  Serial.begin(9600);
+}
+
+float getInputMagnitude(int readPin) {
+  // Get the input magnitude on a given pin
+  return abs(5.0 - (analogRead(readPin) * (5.0 / 1023.0)));
+}
+
+void sendDataOverSerial(int pinNumber, float inputMagnitude) {
+  for (int pinIndex = 0; pinIndex < sizeof(PIN_NUMBERS); pinIndex++) {
+    if (PIN_NUMBERS[pinIndex] == pinNumber) {
+      byte pinIdentifier = PIN_IDENTIFIERS[pinIndex];
+      byte *b = (byte *)&inputMagnitude;
+      
+      Serial.write(pinIdentifier); // Identifier for pin
+      
+      for (int i = 0; i < sizeof(float); i++) {
+        Serial.write(b[i]); // Data
+      }
+      
+      break; // Exit loop after writing data for the specified pin
+    }
+  }
+}
+
+void loop() {
+  // Convert the analog reading to abs diff between 5.0V and input V.
+  inputA0 = getInputMagnitude(A0);
+  inputA1 = getInputMagnitude(A1);
+  inputA3 = getInputMagnitude(A3);
+  inputA4 = getInputMagnitude(A4);
+
+  if (inputA0 > 0.05) {
+    sendDataOverSerial(A0, inputA0);
+  } 
+  if (inputA1 > 0.05) {
+    sendDataOverSerial(A1, inputA1);
+  }
+  if (inputA3 > 0.05) {
+    sendDataOverSerial(A3, inputA3);
+  } 
+  if (inputA4 > 0.05) {
+    sendDataOverSerial(A4, inputA4);
+  }
+
+  // Add a delay to prevent flooding the Serial Monitor with data
+  delay(100); // Adjust delay as needed
+}
